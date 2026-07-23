28431d09bb9a67655dc96ccd01e332bcae376727
SumitVIP25 <prashantsumit08@gmail.com>
2026-07-23 23:00:54 +0530

    Initial project setup

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-700 text-white py-6 mt-20">
+      <div className="text-center">
+        &copy; {new Date().getFullYear()} Sumit Prashant
+      </div>
+    </footer>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-700 text-white shadow">
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-6  ">
+        <Link href="/" className="text-2xl font-bold text-blue-400">
+          DevFolio
+        </Link>
+
+        <div className="flex gap-8">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/projects">Projects</Link>
+          <Link href="/experience">Experience</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
